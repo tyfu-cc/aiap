0dbd2f6c9dfbcf9105e1f4058402d90954dbfa4a
Tianyi Fu <f.tianyi@wustl.edu>
2025-06-04 12:30:42 -0500

    modified density estimation

--- bin/density_estimation.R
+++ bin/density_estimation.R
@@ -7,15 +7,17 @@ outfile <- args[2]
 if (file.info(infile)$size == 0) {
   file.create(outfile)
 } else {
-  raw_counts <- read.table(infile, header = FALSE)
-  expanded_counts <- rep(raw_counts$V1, raw_counts$V2)
-  dens <- density(expanded_counts, bw = "SJ")
-  write.table(
-    data.frame(dens$x, dens$y),
-    file = outfile,
-    sep = "\t",
-    row.names = FALSE,
-    col.names = FALSE,
-    quote = FALSE
-  )
+  if (length(expanded_counts) < 2) {
+    file.create(outfile)
+  } else {
+    dens <- density(expanded_counts, bw = "SJ")
+    write.table(
+      data.frame(dens$x, dens$y),
+      file = outfile,
+      sep = "\t",
+      row.names = FALSE,
+      col.names = FALSE,
+      quote = FALSE
+    )
+  }
 }
